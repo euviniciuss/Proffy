79375ac656d34905713365733a7b5fa9fc1d9940
dev-viniciuss <dev.mvsc@gmail.com>
2020-08-09 23:46:37 -0300

    Mobile da aplicação iniciado

---
+++ mobile/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold, useFonts } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack'; 
+
+export default function App() {
+  let [fontLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if(!fontLoaded) {
+    return <AppLoading />;    
+  } else {
+  return (
+    <>
+      <AppStack />
+      <StatusBar style="light"/>
+    </>
+  );
+} 
+}
